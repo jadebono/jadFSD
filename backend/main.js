a7a9f2e780cba784d60a6d645ae490633df45164
jadebono <joe@jadebono.com>
2022-04-03 11:31:49 +0200

    removed cookie-parser imports

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Imports
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
@@ -22,7 +21,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(cookieParser());
 app.use(logger);
 
 // routes
